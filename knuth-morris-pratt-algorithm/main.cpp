c58193ad027f1f15cef04a4d80eca4720281e0bb
manu <manu@localhost.localdomain>
2022-09-01 20:23:29 -0500

    mkp

--- knuth-morris-pratt-algorithm/main.cpp
+++ knuth-morris-pratt-algorithm/main.cpp
@@ -5,48 +5,63 @@ using namespace std;
 Title: Knuth Morris Pratt Algorithm (KMP)
 */
 
-vector<int> getLPS(string pattern);
-void kmpa(string text, string pattern);
+vector<int> getLPS(string substr);
+void kmpa(string str, string substr);
 
 int main()
 {
-    string text = "hola mundo mun!";
-    string pattern = "munu";
-    //                00101234
+    string str = "Hola mundo mun mun!";
+    string substr = "mun";
 
-
-    kmpa(text, pattern);
+    kmpa(str, substr);
 }
 
-void kmpa(string text, string pattern)
+void kmpa(string str, string substr) // Time: O(N+M)
 {
-    int const tSize = text.length();
-    int const pSize = pattern.length();
-    vector<int> lps;
+    vector<int> lps = getLPS(substr);
+    int i=0;
+    int j=0;
 
-    lps = getLPS(pattern);
-    for (int i = 0; i < pSize; i++)
-        cout << pattern[i] << "  ";
-    cout << endl;
-    for (int i = 0; i < pSize; i++)
-        cout << lps[i] << " ";
-    
-    
+    if(substr.size() > str.size()) cout << "[Error]: Patrón más grande que texto" << endl;
+
+    while(i < str.size())
+    {
+        if(str[i] == substr[j])
+        {
+            if(j == substr.size()-1) cout << "Patrón en index: " << i-2 << endl;
+            i++; j++;
+        }
+        else if(j > 0)
+        {
+            j = lps[j-1]+1;
+        }
+        else
+        {
+            i++;
+        }
+    }
 }
 
-vector<int> getLPS(string pattern)
+vector<int> getLPS(string substr) // Time: O(M)
 {
-    vector<int> lps(pattern.size(),-1);
+    /*
+        Our default lps = { -1, -1, -1, -1}
+        Our Index =       { 0 , 1 , 2 , 3 }
+        Our Substr =      {'m','u','n','u'}
+        Our Lps =         { -1, -1, -1, -1}
+    */
+
+    vector<int> lps(substr.size(),-1);
     int i = 1;
     int j = 0;
-    while(i<pattern.size())
+
+    while(i < substr.size())
     {
-        if(pattern[i] == pattern[j]) lps[i++] = j++;
+        if(substr[i] == substr[j]) lps[i++] = j++;
         else if(j > 0) j = lps[j-1]+1;
         else i++;
     }
-
-       
+   
     return lps;
 }
 
